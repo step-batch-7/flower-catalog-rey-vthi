e0e2edc2571ece4bdaaadc4121a20357d8235f61
revthi@gmail.com <58027828+rey-vthi@users.noreply.github.com>
2020-01-29 17:46:41 +0530

    created a app.js file that have app Class

--- handler.js
+++ handler.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const url = require('url');
+const {App} = require('./app.js');
 const loadTemplate = require('./lib/loadTemplate');
 const CONTENT_TYPES = require('./lib/mime');
 
@@ -101,38 +102,6 @@ const readBody = function(req, res, next) {
   });
 };
 
-class App {
-  constructor() {
-    this.routes = [];
-  }
-  get(path, handler) {
-    this.routes.push({path, handler, method: 'GET'});
-  }
-  post(path, handler) {
-    this.routes.push({path, handler, method: 'POST'});
-  }
-  use(middleware) {
-    this.routes.push({handler: middleware});
-  }
-  processRequest(req, res) {
-    console.log('Request: ', req.url, req.method);
-    const matchingHandlers = this.routes.filter(route =>
-      matchRoute(route, req)
-    );
-    const next = function() {
-      if (matchingHandlers.length === 0) return;
-      const router = matchingHandlers.shift();
-      router.handler(req, res, next);
-    };
-    next();
-  }
-}
-
-const matchRoute = function(route, req) {
-  if (route.method)
-    return req.method == route.method && req.url.match(route.path);
-  return true;
-};
 
 const app = new App();
 
